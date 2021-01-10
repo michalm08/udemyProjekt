325b5536ed78a7677ace0382dec7ef379b0f67c8
michalm08 <michalmski@gmail.com>
2021-01-10 23:10:42 +0100

    Add folder with api

---
+++ config/db.js
@@ -0,0 +1,23 @@
+const mongoose = require('mongoose');
+const config = require('config');
+//tak moge wyciagac sb rzeczy z jsona
+const db = config.get('mongoURI')
+
+// zabezpieczenie z funkcja asynchroniczna i tryCatch jest spoko
+const connectDB = async () =>{
+    try {
+        await mongoose.connect(db, {
+            useNewUrlParser: true,
+            useUnifiedTopology: true
+        });
+
+        console.log('MongoDB Connected...');
+    } catch (err) {
+        console.error(err.message)
+
+        //Exit process with fail
+        process.exit(1);
+    }
+}
+
+module.exports = connectDB;
